9d4dc8ab100a5a7b0b338b0d8b27eff2bb70f718
andjad <88319984+andjad@users.noreply.github.com>
2021-09-20 16:01:52 +0100

    Fix json values (#3)
    Co-authored-by: Amjad <amjad@Amjads-MacBook-Pro.local>

--- apps/backend/opishub/src/app/account.controller.ts
+++ apps/backend/opishub/src/app/account.controller.ts
@@ -17,6 +17,7 @@ import { Parser } from 'xml2js';
 import { generate } from 'randomstring';
 
 const USER_DOC = 'USERS';
+const CREDIT_MULTIPLIER = 0.001;
 
 @Controller('/account')
 export class AccountController {
@@ -40,7 +41,13 @@ export class AccountController {
     await this.firebase.firebaseApp.firestore().collection(USER_DOC).doc(createAccount.uid).set({
       boincUsername: uniqueUsername,
       boincEmail: boincEmail,
-      credit: 10
+      boincCredit: 0,
+      credit: {
+        total: 0,
+        compute: 0,
+        referral: 0,
+        share: 0
+      }
     });
 
     const loggedInUser = await this.firebase.firebaseClient.auth().signInWithEmailAndPassword(newUser.email, newUser.password);
@@ -88,7 +95,7 @@ export class AccountController {
 
   @Get('info')
   @ApiBearerAuth('access-token')
-  async info(@Headers('Authorization') token): Promise<{ tokens: number, amount: number, time: number, avgCredit: number }> {
+  async info(@Headers('Authorization') token) {
     const validUser = await this.authUserToken(token).catch(err => {
       throw new UnauthorizedException(err.message);
     });
@@ -114,10 +121,14 @@ export class AccountController {
     });
 
     return {
-      tokens: parseFloat(validUser.credit),
-      amount: parseFloat(result.user?.total_credit[0]),
-      time: parseInt(result.user?.expavg_time[0]),
-      avgCredit: parseFloat(result.user?.expavg_credit[0])
+      credit: {
+        total: parseFloat(validUser.credit.total),
+        compute: parseFloat(validUser.credit.compute),
+        referral: parseFloat(validUser.credit.referral),
+        share: parseFloat(validUser.credit.share),
+      },
+      time: this.secondsToDhms(result.user?.expavg_time[0]),
+      estCredit: parseFloat(result.user?.expavg_credit[0]) * CREDIT_MULTIPLIER
     };
   }
 
@@ -155,6 +166,20 @@ export class AccountController {
     const data = validUser.data();
     data.uid = loggedInUser.uid;
     return data;
+  }
+
+  secondsToDhms(seconds) {
+    seconds = Number(seconds);
+    const d = Math.floor(seconds / (3600*24));
+    const h = Math.floor(seconds % (3600*24) / 3600);
+    const m = Math.floor(seconds % 3600 / 60);
+    const s = Math.floor(seconds % 60);
+
+    const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+    const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
   }
 
 }
