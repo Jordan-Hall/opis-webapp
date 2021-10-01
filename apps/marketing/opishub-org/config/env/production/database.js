579bca43413279e2023167b742f61ee2531877a1
Jordan Hall <jordan@libertyware.co.uk>
2021-10-01 13:58:55 +0100

    feat: producation env
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

---
+++ apps/marketing/opishub-org/config/env/production/database.js
@@ -0,0 +1,23 @@
+import { parse } from 'pg-connection-string';
+const config = parse(process.env.DATABASE_URL);
+
+export default () => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {},
+    },
+  },
+});
---
+++ apps/marketing/opishub-org/config/env/production/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9e14bd02929b150a3424a7f58202488c'),
+    },
+  },
+});
